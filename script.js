1a5158ae4dcb194b1ea67efabf75e280e002f0ab
arjunbaniya <arjunbaniya2047@gmail.com>
2024-04-05 23:43:59 +0100

    first commit2

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-function showit(){
-    var theimg = document.getElementById("myimage");
-    var thetext = document.getElementById("myoption").value;
+function genQR() {
+   var gapi="https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
+            var myimg=document.getElementById("img");
+            var mytext=document.getElementById("qrtext").value;
+            var mysize=document.getElementById("size").value;
 
-    alert(thetext);
+            if (mytext!=="" && mysize=="100")
+            {
+                myimg.src=gapi+"100x100"+"&chl="+mytext;
+                // https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=hello
+            }
 
-    if (thetext === "google") {
-        theimg.src = "https://www.gstatic.com/webp/gallery3/1.png";
-    }
-    else if (thetext === "bing") {
-        theimg.src = "https://www.gstatic.com/webp/gallery3/1.png";
-    }
-    else if (thetext === "facebook") {
-        theimg.src = "https://www.gstatic.com/webp/gallery3/1.png";
-    }
-    else {
-        alert("Picture is not available");
-    }
+            else if(mytext!=="" && mysize=="150")
+            {
+
+                myimg.src=gapi+"150x150"+"&chl="+mytext;
+            }
+
+            else if(mytext!=="" && mysize=="200")
+            {
+                myimg.src=gapi+"200x200"+"&chl="+mytext;
+            }
+
+            else if (mytext!=="" && mysize=="250")
+            {
+                myimg.src=gapi+"250x250"+"&chl="+mytext;
+            }
+
+            else if (mytext!=="" && mysize=="300")
+            {
+                myimg.src=gapi+"300x300"+"&chl="+mytext;
+            }
+
+            else
+            {
+                alert("Please Enter Text");
+            }
+        
+        
 }
